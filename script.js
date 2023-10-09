04e77bcbe7b187ccb30f4c49564576bb30164801
architvohra123 <archit0249.be21@chitkara.edu.in>
2023-10-09 18:19:41 +0530

    adding local storage

--- script.js
+++ script.js
@@ -1,83 +1,93 @@
-var selectedRow = null;
+// function to check if form is valid or not
+function validateForm(){
+    var name = document.getElementById("name").value;
+    var age = document.getElementById("age").value;
+    var address = document.getElementById("address").value;
+    var email = document.getElementById("email").value;
+    
+    if(name == ""){
+        alert("Name is required");
+        return false;
+    }
+    if(age == ""){
+        alert("Age is required");
+        return false;
+    }
+    else if(age<1){
+        alert("Age cannot be zero or less");
+        return false;
+    }
+    if(address==""){
+        alert("Address is required");
+        return false;
+    }
+    if(email==""){
+        alert("Email is required");
+        return false;
+    }
+    else if(!email.includes("@")){
+        alert("Enter valid email address");
+        return false;
+    }
 
-//show alerts
-function showAlert(message, className){
-    const div = document.createElement("div");
-    div.className = `alert alert-${className}`;
-
-    div.appendChild(document.createTextNode(message));
-    const container = document.querySelector(".container");
-    const main = document.querySelector(".main");
-    container.insertBefore(div, main);
-
-    setTimeout(() => document.querySelector(".alert").remove(), 3000);
+    return true;
 }
 
-//clear all fields
-function clearFields(){
-    document.querySelector("#firstName").value= "";
-    document.querySelector("#lastName").value= "";
-    document.querySelector("#rollNo").value= "";
+// function to show data
+function showData(){
+    var peopleList;
+    if(localStorage.getItem("peopleList") == null){
+        peopleList = [];
+    }
+    else{
+        peopleList = JSON.parse(localStorage.getItem("peopleList"));
+    }
+
+    var html="";
+
+    peopleList.forEach(function (element, index){
+        html += "<tr>";
+        html += "<td>" + element.name + "</td>";
+        html += "<td>" + element.age + "</td>";
+        html += "<td>" + element.address + "</td>";
+        html += "<td>" + element.email + "</td>";
+        html += '<td><button onclick="deleteData(' + index + ')" class="btn btn-danger">Delete</button><button onclick="updateData(' + index + ')" class="btn btn-warning m-2">Edit</button></td>'
+        html += "</tr>";
+    });
+    document.querySelector(".tbody").innerHTML = html;
 }
 
-//Add data
-document.querySelector("#student-form").addEventListener("submit", (e)=>{
-    e.preventDefault();
+//loads all data when document or page load
+document.onload = showData();
 
-    //get Form Values
-    const firstName = document.querySelector("#firstName").value;
-    const lastName = document.querySelector("#lastName").value;
-    const rollNo = document.querySelector("#rollNo").value;
+//function to add data
+function AddData(){
+    //if form is valid
+    if(validateForm() == true){
+        var name = document.getElementById("name").value;
+        var age = document.getElementById("age").value;
+        var address = document.getElementById("address").value;
+        var email = document.getElementById("email").value;
 
-    //validation
-    if(firstName == "" || lastName == "" || rollNo == ""){
-        showAlert("Please fill in all fields", "danger");
-    }else{
-        if(selectedRow == null){
-            const list = document.querySelector("#student-list");
-            const row = document.createElement("tr");
-
-            row.innerHTML = `
-                <td>${firstName}</td>
-                <td>${lastName}</td>
-                <td>${rollNo}</td>
-                <td>
-                <a href="#" class="btn btn-warning btn-sm edit">Edit</a>
-                <a href="#" class="btn btn-danger btn-sm delete">Delete</a>
-                </td>
-            `;
-            list.appendChild(row);
-            selectedRow = null;
-            showAlert("Student Added", "success");
+        var peopleList;
+        if(localStorage.getItem("peopleList") == null){
+            peopleList = [];
         }else{
-            selectedRow.children[0].textContent = firstName;
-            selectedRow.children[1].textContent = lastName;
-            selectedRow.children[2].textContent = rollNo;
-            selectedRow=null;
-            showAlert("Student Info Edited", "info");
+            peopleList = JSON.parse(localStorage.getItem("peopleList"));
         }
-
-        clearFields();
+        
+        peopleList.push({
+            name : name,
+            age: age,
+            address: address,
+            email: email,
+        })
+        localStorage.setItem("peopleList", JSON.stringify(peopleList));
+        showData();
+        document.getElementById("name").value = "";
+        document.getElementById("age").value = "";
+        document.getElementById("address").value = "";
+        document.getElementById("email").value = "";
+        
     }
-});
-
-//edit data
-
-document.querySelector("#student-list").addEventListener("click", (e)=>{
-    target = e.target;
-    if(target.classList.contains("edit")){
-        selectedRow = target.parentElement.parentElement;
-        document.querySelector("#firstName").value = selectedRow.children[0].textContent;
-        document.querySelector("#lastName").value = selectedRow.children[1].textContent;
-        document.querySelector("#rollNo").value = selectedRow.children[2].textContent;
-    } 
-});
-
-//delete data
-document.querySelector("#student-list").addEventListener("click", (e)=>{
-    target = e.target;
-    if(target.classList.contains("delete")){
-        target.parentElement.parentElement.remove();
-        showAlert("Student data deleted", "danger");
-    }
-});
+}
